aee9d2fa029feb032e7ebdf05b512797bc521593
Teddy Bandama <tebandam@1H1.42angouleme.fr>
2024-03-27 12:43:52 +0100

    todo ft_strlen

--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 /*   By: tebandam <tebandam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 09:31:43 by tebandam          #+#    #+#             */
-/*   Updated: 2024/03/27 10:18:33 by tebandam         ###   ########.fr       */
+/*   Updated: 2024/03/27 12:43:26 by tebandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
+	// si il y a rien dedans ?
 	programme doit recevoir 4 ou 5 arguments 
 	Analysons l'entrée que nous recevrons : 5 800 200 200 7
 	5 — Le nombre de philosophes
@@ -26,10 +27,49 @@
 
 #include "philosophers.h"
 
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+#include <stdio.h>
 int	main(int argc, char **argv)
 {
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 1;
 	if (argc < 5)
+	{
 		ft_putstr_fd("Error\nNumber of arguments is wrong\n", 2);
-	(void)argv;
-			
+		return (1);
+	}
+	while (argv[j])
+	{	
+		printf("%s\n", argv[j]);
+		while (argv[j][i])
+		{
+			if (ft_isdigit(argv[j][i]) == 0)
+			{	
+				ft_putstr_fd("Error\nPlease write only numbers\n", 2);		
+				return(1);
+			}
+			i++;
+		}
+		i = 0;
+		j++;
+	}
 }
