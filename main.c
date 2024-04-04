e8d361e605358c6d5c127435acb97fdf060273b2
Teddy Bandama <tebandam@1F4.42angouleme.fr>
2024-04-04 19:15:26 +0200

    added .h

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tebandam <tebandam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 09:31:43 by tebandam          #+#    #+#             */
-/*   Updated: 2024/03/31 13:38:38 by tebandam         ###   ########.fr       */
+/*   Updated: 2024/04/04 17:13:04 by tebandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,20 @@ int	incorrect_number_arguments(int argc)
 
 int	main(int argc, char **argv)
 {
-	incorrect_number_arguments(argc);
 	validate_arguments(argv);
+	incorrect_number_arguments(argc);
+	
+	t_philo philo;
+
+
+
+
+
+
+
+	
+	pthread_t	thread1;
+
+	philo = malloc(sizeof(t_philo));
+	pthread_create(&thread1, NULL, NULL, NULL);
 }
--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: tebandam <tebandam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 09:40:53 by tebandam          #+#    #+#             */
-/*   Updated: 2024/03/31 15:39:44 by tebandam         ###   ########.fr       */
+/*   Updated: 2024/04/04 18:46:18 by tebandam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,77 @@
 # define PHILOSOPHERS_H
 #include <cstddef>
 # include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
 # define EMPTY '\0'
 
-typedef struct{
-	// pthread_t		thread;
 
-	// Chaque philosophe possède un numéro allant de 1 ànombre_de_philosophes.
+typedef struct {
+	int	id_fork;
+	pthread_mutex_t *is_fork_used;
+} t_fork;
+
+
+
+typedef struct {
 	int	number_of_philosophers;
 	int number_of_times_each_philosopher_must_eat;
 	size_t	time_to_die;
 	size_t	time_to_eat;
 	size_t	time_to_sleep;
-	//pthread_mutex_t	*r_fork;
-	//pthread_mutex_t	*l_fork;
 	
+} t_prog;
+
+
+
+
+
+typedef struct {
+	int	id_philo;
+	t_fork *right_fork;
+	t_fork *left_fork;
+	pthread_mutex_t	*right_fork;
+	pthread_mutex_t	*left_fork;
 } t_philo;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// typedef struct{
+// 	// pthread_t		thread;
+
+// 	// Chaque philosophe possède un numéro allant de 1 ànombre_de_philosophes.
+	
+// } t_philo;
 
 // typedef struct{
 // 	int dead_flag;
